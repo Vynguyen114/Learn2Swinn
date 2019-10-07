3634673389fbd2ff4c9f760381fcbcce3d5bc3b8
Andrew1778 <54136622+andrew1778@users.noreply.github.com>
2019-10-07 16:55:17 +1100

    Add files via upload

---
+++ C#src/src/UnitTest/GameControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+
+namespace GameControllerTest
+{
+    [TestClass()]
+    public class GameControllerTests
+    {
+        [TestMethod()]
+        public void CreateComputerPlayer_hardSelected()
+        {
+            GameController.SetDifficulty(AIOption.Hard);
+            GameController.CreateComputerPlayer();
+            Assert.IsInstanceOfType(GameController.ComputerPlayer, typeof(AIHardPlayer));
+        }
+        [TestMethod()]
+        public void CreateComputerPlayer_mediumSelected()
+        {
+            GameController.SetDifficulty(AIOption.Medium);
+            GameController.CreateComputerPlayer();
+            Assert.IsInstanceOfType(GameController.ComputerPlayer, typeof(AIMediumPlayer));
+        }
+        [TestMethod()]
+        public void CreateComputerPlayer_easySelected()
+        {
+            GameController.SetDifficulty(AIOption.Easy);
+            GameController.CreateComputerPlayer();
+            Assert.IsInstanceOfType(GameController.ComputerPlayer, typeof(AIEasyPlayer));
+        }
+    }
+}
