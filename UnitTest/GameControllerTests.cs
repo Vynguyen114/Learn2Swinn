1136fff2c7186d6e89e0c91798113b5a6f723d64
Vy <54136953+janyoko123@users.noreply.github.com>
2019-10-09 16:33:46 +1100

    Vy's UnitTesting

---
+++ UnitTest/GameControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    [TestClass()]
+    public class VyTest
+    {
+        //Test adding new game state
+        [TestMethod()]
+        public void TestGameState()
+        {
+            GameState state = GameState.Deploying;
+            GameController.AddNewState(state);
+
+            Assert.AreEqual(GameController.CurrentState, state);
+        }
+
+
+        //Test to remove old game state
+        [TestMethod()]
+        public void TestEndCurrentState()
+        {
+            GameState state1 = GameState.Deploying;
+            GameState state2 = GameState.Discovering;
+
+            GameController.AddNewState(state1);
+            GameController.AddNewState(state2);
+
+            GameController.EndCurrentState();
+
+            Assert.AreEqual(GameController.CurrentState, state1);
+
+
+
+        }
+    }
+}
