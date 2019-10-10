9c37ae3fe53f66f2ae3ebaad0dad67c9dc9ff22b
AshenFonseka <54136895+AshenFonseka@users.noreply.github.com>
2019-10-10 18:57:15 +1100

    Add files via upload

---
+++ UnitTest/PlayerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {        
+        [TestMethod()]
+        public void AItest()
+        {
+            string i;
+            GameController.SetDifficulty(AIOption.Easy);
+            GameController.SetDifficulty(AIOption.Medium);
+            GameController.CreateComputerPlayer();
+
+            i = GameController.ComputerPlayer.ToString();
+            string expected = "AIMediumPlayer";
+            Assert.AreEqual(expected, i);
+        }
+        [TestMethod()]
+        public void ShipNumberTest()
+        {
+            int i = 0;
+            BattleShipsGame S0 = new BattleShipsGame();
+            var s0 = new Player(S0);
+            foreach (ShipName shipToPlace in Enum.GetValues(typeof(ShipName)))
+            {
+                if (shipToPlace != ShipName.None)
+                {
+                    i++;
+                    continue;
+                }
+            }
+            int expected = 5;
+            Assert.AreEqual(expected, i);
+        }
+
+    }
+}
